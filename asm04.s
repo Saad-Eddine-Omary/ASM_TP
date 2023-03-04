3fb53cc6c2990e99003c9358ab2258b2b7f5558d
Saad-Eddine-Omary <saad.eddine.omary12@gmail.com>
2023-03-04 15:52:10 +0100

    works for 1 digit number

--- asm04.s
+++ asm04.s
@@ -1,30 +1,59 @@
 global _start
 
+section .data
+    odd db "1", 10
+    even db "0", 10
+
 section .bss
 
-    x resb  4
+    x resq  4
 
 section .text
+
 _start:
     ;scanf
-    mov eax, 3;
-    mov ebx, 0;stdin
-    mov ecx, x;pointer
-    mov edx, 5
+    mov rax, 3;
+    mov rbx, 0;stdin
+    mov rcx, x;pointer
+    mov rdx, 5
     int 80h
 
-    mov eax,[ecx]
-    mov ebx, 2
-    div ebx
-    cmp ah, 0
-    jne odd
+    mov rcx, [rcx]
+    sub cl, 0x30
+    cmp ch, 0x0a
+    je clean_buff
 
-    mov eax,1
-    mov ebx,0
+
+
+clean_buff:
+    xor ch,ch
+    xor bl,bl
+    mov al, cl
+    mov bl, 0x02
+    div bl
+    cmp ah,0
+    jne _odd
+    mov eax, 4;
+    mov ebx, 1;stdout
+    mov ecx, even
+    mov edx, 2
+    int 80h
+
+    mov eax, 1; return 0
+    mov ebx, 0;
+    int 80h;
+
+
+_odd:
+    mov eax, 4;
+    mov ebx, 1;stdout
+    mov ecx, odd
+    mov edx, 2
     int 80h
 
 
-odd:
-    mov eax,1
-    mov ebx,1
-    int 80h
+
+    mov eax, 1; return 0
+    mov ebx, 0;
+    int 80h;
+
